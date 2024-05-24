712c69fe185a9db12523014d373362e661f7700e
gleb <git config --global user.email>
2024-05-24 18:10:23 +0000

    really cool file

--- src/Main.java
+++ src/Main.java
@@ -23,20 +23,19 @@ public class Main {
 //        System.out.println(questions.Q6(s));
 //        System.out.println(questions.Q7(s));
 //        Q8                           my fav one :)
-        if (questions.Q8(s,s2)){
-            System.out.println("is anagram");
-        }else {
-            System.out.println("isn't anagram");
-        }
+//        if (questions.Q8(s,s2)){
+//            System.out.println("is anagram");
+//        }else {
+//            System.out.println("isn't anagram");
+//        }
     }
 
-
+    //Tapes
     //s.equals(s2); - checks if variables are same inside.
     //s.contains("word"); - checks if there is any WORD inside of text, word to search sets inside of function.
     //s.contains('w'); - checks if there is any CHARACTER inside of text, character to search sets inside of function.
     //s.indexOf("word"); - checks if there is word what is needed to search, if there is so function will return idex where that word is.
     //s.toLowerCase/toUpperCase(); - changes string to lower/upper case.
     //s.split(""); - gives array with words, inside of function is the split element.
-    //s.substring(0,1); - cuts space beetween ids inside function.
-
+    //s.substring(0,1); - cuts space between ids inside function.
 }
