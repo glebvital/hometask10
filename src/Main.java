e614e723205861d0e305bc21d94c2d0c5415a977
gleb <git config --global user.email>
2024-05-24 18:08:56 +0000

    really cool file

--- src/Main.java
+++ src/Main.java
@@ -2,104 +2,34 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
+        Questions questions = new Questions();
         String s = "hello world!";
         String s2 = "rob";
 //        Q0
-//        System.out.println(wordcount(s));
-//        System.out.println(Q1(s));
-//        System.out.println(Q2(s));
+//        System.out.println(questions.wordcount(s));
+
+//        System.out.println(questions.Q1(s));
+//        System.out.println(questions.Q2(s));
+
 //        Q3
-//        if (Q3(s)){
+//        if (questions.Q3(s)){
 //            System.out.println("polidrom");
 //        }else {
 //            System.out.println("not polidrom");
 //        }
 
-//        System.out.println(Q4(s));
-//        Q5(s);
-//        System.out.println(Q6(s));
-//        System.out.println(Q7(s));
+//        System.out.println(questions.Q4(s));
+//        questions.Q5(s);
+//        System.out.println(questions.Q6(s));
+//        System.out.println(questions.Q7(s));
 //        Q8                           my fav one :)
-//        if (Q8(s,s2)){
-//            System.out.println("is anagram");
-//        }else {
-//            System.out.println("isn't anagram");
-//        }
-    }
-    public static int wordcount(String s){
-        int count = s.split("\\s").length;
-        return count;
-    }
-    public static String Q1(String s){
-        String reversed = "";
-        for (int i = s.length()-1; i >= 0 ; i--) {
-            reversed += s.charAt(i);
-        }
-        return reversed;
-    }
-    public static int Q2(String s){
-        s.toLowerCase();
-        int count = 0;
-        for (int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
-            if (c=='a'||c=='e'||c=='i'||c=='o'||c=='u'){
-                count++;
-            }
-        }
-        return count;
-    }
-    public static boolean Q3(String s){
-        s = s.toLowerCase().replaceAll("//s","");
-        for (int i = 0; i < s.length() / 2; i++) {
-            if (s.charAt(i) != s.charAt(s.length() - i - 1)){
-                return false;
-            }
-        }
-        return true;
-    }
-    public static String Q4(String s){
-        Set<Character> charset = new LinkedHashSet<>();
-        for (char c : s.toCharArray()) {
-            charset.add(c);
-        }
-        StringBuilder result = new StringBuilder();
-        for (char c : charset) {
-            result.append(c);
-        }
-        return result.toString();
-    }
-    public static void Q5(String s){
-        String[] ar = s.split(" ");
-        for (int i = 0; i < ar.length; i++) {
-            System.out.print(ar[i]+" ");
+        if (questions.Q8(s,s2)){
+            System.out.println("is anagram");
+        }else {
+            System.out.println("isn't anagram");
         }
     }
-    public static String Q6(String s){//that question already was before, but i'll try it in another way ;)
-        StringBuilder reverse = new StringBuilder(s);
-        reverse.reverse();
-        return reverse.toString();
-    }
-    public static String Q7(String s){
-        String[] ar = s.split("\\s");
-        StringBuilder builder = new StringBuilder();
-        for (String word : ar){
-            if(!word.isEmpty()){
-                builder.append(Character.toUpperCase(word.charAt(0)));
-                builder.append(word.substring(1));
-                builder.append(" ");
 
-            }
-        }
-        return builder.toString();
-    }
-    public static boolean Q8(String s,String s2){
-
-        char[] sAr = s.toCharArray();
-        char[] sAr2 = s2.toCharArray();
-        Arrays.sort(sAr);
-        Arrays.sort(sAr2);
-        return Arrays.equals(sAr,sAr2);
-    }
 
     //s.equals(s2); - checks if variables are same inside.
     //s.contains("word"); - checks if there is any WORD inside of text, word to search sets inside of function.
---
+++ src/Questions.java
@@ -0,0 +1,80 @@
+import java.util.Arrays;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class Questions {
+    public int wordcount(String s){
+        int count = s.split("\\s").length;
+        return count;
+    }
+    public String Q1(String s){
+        String reversed = "";
+        for (int i = s.length()-1; i >= 0 ; i--) {
+            reversed += s.charAt(i);
+        }
+        return reversed;
+    }
+    public int Q2(String s){
+        s.toLowerCase();
+        int count = 0;
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c=='a'||c=='e'||c=='i'||c=='o'||c=='u'){
+                count++;
+            }
+        }
+        return count;
+    }
+    public boolean Q3(String s){
+        s = s.toLowerCase().replaceAll("//s","");
+        for (int i = 0; i < s.length() / 2; i++) {
+            if (s.charAt(i) != s.charAt(s.length() - i - 1)){
+                return false;
+            }
+        }
+        return true;
+    }
+    public String Q4(String s){
+        Set<Character> charset = new LinkedHashSet<>();
+        for (char c : s.toCharArray()) {
+            charset.add(c);
+        }
+        StringBuilder result = new StringBuilder();
+        for (char c : charset) {
+            result.append(c);
+        }
+        return result.toString();
+    }
+    public void Q5(String s){
+        String[] ar = s.split(" ");
+        for (int i = 0; i < ar.length; i++) {
+            System.out.print(ar[i]+" ");
+        }
+    }
+    public String Q6(String s){//that question already was before, but i'll try it in another way ;)
+        StringBuilder reverse = new StringBuilder(s);
+        reverse.reverse();
+        return reverse.toString();
+    }
+    public String Q7(String s){
+        String[] ar = s.split("\\s");
+        StringBuilder builder = new StringBuilder();
+        for (String word : ar){
+            if(!word.isEmpty()){
+                builder.append(Character.toUpperCase(word.charAt(0)));
+                builder.append(word.substring(1));
+                builder.append(" ");
+
+            }
+        }
+        return builder.toString();
+    }
+    public boolean Q8(String s,String s2){
+
+        char[] sAr = s.toCharArray();
+        char[] sAr2 = s2.toCharArray();
+        Arrays.sort(sAr);
+        Arrays.sort(sAr2);
+        return Arrays.equals(sAr,sAr2);
+    }
+}
